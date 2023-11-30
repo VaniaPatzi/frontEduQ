81395f3a20d4d02f43cf6b6f456169f613d1640b
John Cler <choquejohn6@gmail.com>
2023-11-30 16:07:05 -0400

    Modificando menu de opciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './component/inicio/inicio.component';
+import { RegistroNotasComponent } from './component/registro-notas/registro-notas.component';
 
 const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
+  {path: 'registro-notas', component: RegistroNotasComponent}
 ];
 
 @NgModule({
