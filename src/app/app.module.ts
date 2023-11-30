1402116af35391e64486b1cb70b0322eb9c6606f
VanPam1 <vanpamep.q@gmail.com>
2023-11-30 14:06:36 -0400

    generando el componente registro de notas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './component/inicio/inicio.component';
+import { RegistroNotasComponent } from './component/registro-notas/registro-notas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    RegistroNotasComponent
   ],
   imports: [
     BrowserModule,
