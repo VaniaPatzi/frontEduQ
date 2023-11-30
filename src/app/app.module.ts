e07db10ad399cba23bad854cc1030a71a2a0357d
VanPam1 <vanpamep.q@gmail.com>
2023-11-30 14:58:08 -0400

    generando el componente de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './component/inicio/inicio.component';
 import { RegistroNotasComponent } from './component/registro-notas/registro-notas.component';
 import { MenuComponent } from './component/menu/menu.component';
+import { RegistroComponent } from './component/registro-notas/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     RegistroNotasComponent,
-    MenuComponent
+    MenuComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
