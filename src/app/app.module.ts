f05ae96fc61cb59ab115a738f36c9307dd0b660d
VanPam1 <vanpamep.q@gmail.com>
2023-11-30 14:15:23 -0400

    generando el componente menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './component/inicio/inicio.component';
 import { RegistroNotasComponent } from './component/registro-notas/registro-notas.component';
+import { MenuComponent } from './component/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    RegistroNotasComponent
+    RegistroNotasComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
