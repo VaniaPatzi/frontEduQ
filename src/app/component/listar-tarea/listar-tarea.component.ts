dbea58fe3e278701652c9fc5463785d508923b8b
VanPam1 <vanpamep.q@gmail.com>
2023-12-12 05:32:34 -0400

    coorigiendo lista tareas

--- src/app/component/listar-tarea/listar-tarea.component.ts
+++ src/app/component/listar-tarea/listar-tarea.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class ListarTareaComponent {
 
+  materias: any[] = [];
+  nuevaMateria: any = {};
+  notaMasBaja: number | undefined;
+
+  agregarMateria() {
+    this.materias.push({ ...this.nuevaMateria });
+    this.nuevaMateria = {};
+    this.actualizarNotaMasBaja();
+  }
+
+  eliminarMateria(index: number) {
+    this.materias.splice(index, 1);
+    this.actualizarNotaMasBaja();
+  }
+
+  private actualizarNotaMasBaja() {
+    if (this.materias.length > 0) {
+      this.notaMasBaja = Math.min(...this.materias.map(materia => materia.nota));
+    } else {
+      this.notaMasBaja = undefined;
+    }
+  }
 }
