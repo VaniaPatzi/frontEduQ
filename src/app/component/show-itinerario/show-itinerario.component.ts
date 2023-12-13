08efc65efb7640a8fb0e10b4ed27d036fa6d2ba9
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 14:22:22 -0400

    agregando la lista de itinerario

--- src/app/component/show-itinerario/show-itinerario.component.ts
+++ src/app/component/show-itinerario/show-itinerario.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GradoUsuario } from 'src/app/models/grado_usuario';
+import { Itinerario } from 'src/app/models/itinerario';
+import { GradoService } from 'src/app/Services/grado/grado.service';
+
+import { ItinerarioService } from 'src/app/Services/itinerario/itinerario.service';
 
 @Component({
   selector: 'app-show-itinerario',
   templateUrl: './show-itinerario.component.html',
-  styleUrls: ['./show-itinerario.component.css']
+  styleUrls: ['./show-itinerario.component.css'],
 })
 export class ShowItinerarioComponent {
+  id: string | null = '';
+  oItinerario: Itinerario = new Itinerario();
+  oGrado: GradoUsuario = new GradoUsuario();
+  constructor(
+    private route: ActivatedRoute,
+    public itinerario_service: ItinerarioService,
+    public grado_service: GradoService
+  ) {}
 
+  ngOnInit(): void {
+    let id_url = this.route.snapshot.paramMap.get('id');
+    this.id = id_url ? id_url : '0';
+    this.getItinerarioById(parseInt(this.id));
+  }
+
+  getItinerarioById(id: number) {
+    this.itinerario_service.getItinerarioById(id).subscribe(
+      (response) => {
+        this.oItinerario = response;
+        this.getGradoById(this.oItinerario.usuario_grado_id);
+      },
+      (error) => {}
+    );
+  }
+
+  getGradoById(id: number) {
+    this.grado_service.getGradoById(id).subscribe((response) => {
+      this.oGrado = response;
+    });
+  }
 }
