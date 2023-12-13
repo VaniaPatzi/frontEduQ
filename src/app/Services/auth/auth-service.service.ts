293b4c887776b4119c042e896ddda2d1f95f6c22
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 13:06:46 -0400

    completando el auth

--- src/app/Services/auth/auth-service.service.ts
+++ src/app/Services/auth/auth-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  constructor(private auth: AuthService) {}
+
+  isAuthenticated$(): Observable<boolean> {
+    return this.auth.isAuthenticated$;
+  }
 }
