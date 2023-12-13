d6c3c553618461d370032837172dd7f04edfee9b
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 05:26:17 -0400

    agregando el m materia

---
+++ src/app/models/materia.ts
@@ -0,0 +1,13 @@
+
+export class Materia {
+    id: number;
+    nombre: String;
+    nota: number;
+    estado: String;
+    constructor() {
+      this.id = 0;
+      this.nombre = '';
+      this.nota = 0;
+      this.estado = '';
+    }
+  }
