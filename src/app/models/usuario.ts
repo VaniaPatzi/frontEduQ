f6f03bd7448e377c10f7126f1070b0cceb17c570
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 05:19:38 -0400

    agregando el m usuario

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,11 @@
+export class Usuario {
+    id: number;
+    nombre: String;
+    email: String;
+    constructor() {
+      this.id = 0;
+      this.nombre = '';
+      this.email = '';
+    }
+  }
+  
