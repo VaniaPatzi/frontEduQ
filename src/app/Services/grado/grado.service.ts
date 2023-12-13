a2d4fd7f764cdb95e5bbaa63bb5f897a8010abc2
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 14:32:22 -0400

    agregando el servicio de grado

--- src/app/Services/grado/grado.service.ts
+++ src/app/Services/grado/grado.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GradoUsuario } from 'src/app/models/grado_usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GradoService {
 
-  constructor() { }
+  private API_SERVER = 'http://localhost:8080/api/v1/grados';
+  constructor(private httpClient: HttpClient) {}
+
+  public getGradosUsuario(id: number) {
+    return this.httpClient.get<GradoUsuario[]>(
+      this.API_SERVER + '/grados_usuario/' + id
+    );
+  }
+
+  public getGradoById(id: number) {
+    return this.httpClient.get<GradoUsuario>(this.API_SERVER + '/' + id);
+  }
+
+  public saveRegistro(datos: any): Observable<any> {
+    return this.httpClient.post<GradoUsuario>(this.API_SERVER, datos);
+  }
 }
