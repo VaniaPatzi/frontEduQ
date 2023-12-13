da442571d4293f47d860049386410a53acb1dd6d
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 14:15:58 -0400

    modificando el itinerario

--- src/app/Services/itinerario/itinerario.service.ts
+++ src/app/Services/itinerario/itinerario.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ItinerarioService {
+  saveRegistro(datos: { materia_id: any; materia: import("../../models/materia").Materia; usuario_grado_id: number; res_itinerario: string; tema: string; }) {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
