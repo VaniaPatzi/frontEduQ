e91f65c808c213daa7d4f888c8e11ef6b9dfa004
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 14:37:04 -0400

    agregando el servicio de itinerario

--- src/app/Services/itinerario/itinerario.service.ts
+++ src/app/Services/itinerario/itinerario.service.ts
@@ -1,12 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Itinerario } from 'src/app/models/itinerario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItinerarioService {
-  saveRegistro(datos: { materia_id: any; materia: import("../../models/materia").Materia; usuario_grado_id: number; res_itinerario: string; tema: string; }) {
-    throw new Error('Method not implemented.');
+  private API_SERVER = 'http://localhost:8080/api/v1/itinerarios';
+  constructor(private httpClient: HttpClient) {}
+
+  public getItinerarioByMateria(id: number) {
+    return this.httpClient.get<Itinerario>(
+      this.API_SERVER + '/itinerario_materia/' + id
+    );
+  }
+  public getItinerarioByGrado(id: number) {
+    return this.httpClient.get<Itinerario[]>(
+      this.API_SERVER + '/itinerarios_grado/' + id
+    );
   }
 
-  constructor() { }
+  public getItinerarioById(id: number) {
+    return this.httpClient.get<Itinerario>(this.API_SERVER + '/' + id);
+  }
+
+  public saveRegistro(datos: any): Observable<any> {
+    return this.httpClient.post(this.API_SERVER, datos, {
+      responseType: 'text',
+    });
+  }
 }
