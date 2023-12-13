3754821c9b840826e6063e1b65b12d8a714ec07f
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 17:03:48 -0400

    agregando servicios de materia

--- src/app/Services/materias/materia.service.ts
+++ src/app/Services/materias/materia.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Materia } from 'src/app/models/materia';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MateriaService {
 
-  constructor() { }
+  private API_SERVER = 'http://localhost:8080/api/v1/materias';
+  constructor(private httpClient: HttpClient) {}
+
+  public getMateriasPorGradoUsuario(id: number) {
+    return this.httpClient.get<Materia[]>(
+      this.API_SERVER + '/materias_grado/' + id
+    );
+  }
 }
