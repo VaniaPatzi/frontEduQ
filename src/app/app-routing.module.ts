4e170b5880dec63b4907b5a7dfa342740719bd94
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 17:30:38 -0400

    ruteando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,16 @@ import { InicioComponent } from './component/inicio/inicio.component';
 import { MenuComponent } from './component/menu/menu.component';
 import { MenuBarComponent } from './component/menu-bar/menu-bar.component';
 import { IntroduccionComponent } from './component/introduccion/introduccion.component';
+import { LoginComponent } from './component/login/login.component';
+import { ShowItinerarioComponent } from './component/show-itinerario/show-itinerario.component';
+import { ItinerarioComponent } from './component/itinerario/itinerario.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
   {path: 'inicio', component: InicioComponent},
+  { path: 'itinerario/:id', component: ItinerarioComponent },
+  { path: 'itinerario/show/:id', component: ShowItinerarioComponent },
   {path: 'menuBar', component: MenuBarComponent},
   {path: 'menu', component: MenuComponent},
   {path: 'intro', component: IntroduccionComponent},
