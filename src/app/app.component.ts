cbfc23b69c1e1cbdcfd9cf9b7251c7872420679f
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 18:19:35 -0400

    modificando la pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { AuthServiceService } from './Services/auth/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontEDU';
+  isAuthenticated: boolean = false;
+  cargando: boolean = true;
+
+  constructor(
+    public auth: AuthService,
+    private authService: AuthServiceService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.authService.isAuthenticated$().subscribe((isAuthenticated) => {
+      this.isAuthenticated = isAuthenticated;
+      if (!this.isAuthenticated) {
+        this.router.navigate(['/login']);
+      }
+      this.auth.idTokenClaims$.subscribe((tokenClaims) => {
+        if (tokenClaims && tokenClaims.__raw) {
+          localStorage.setItem('id_token', tokenClaims.__raw);
+        } else {
+          localStorage.removeItem('id_token');
+        }
+      });
+
+      setTimeout(() => {
+        this.cargando = false;
+      }, 500);
+    });
+  }
   
 }
--- src/app/component/introduccion/introduccion.component.ts
+++ src/app/component/introduccion/introduccion.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { AuthServiceService } from 'src/app/Services/auth/auth-service.service';
+import { GradoService } from 'src/app/Services/grado/grado.service';
+import { GradoUsuario } from 'src/app/models/grado_usuario';
 
 @Component({
   selector: 'app-introduccion',
@@ -7,4 +12,37 @@ import { Component } from '@angular/core';
 })
 export class IntroduccionComponent {
 
+  isAuthenticated: boolean = false;
+  listGradosUsuario: GradoUsuario[] = [];
+
+  constructor(
+    public auth: AuthService,
+    private authService: AuthServiceService,
+    public grado_service: GradoService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.authService.isAuthenticated$().subscribe((isAuthenticated) => {
+      this.isAuthenticated = isAuthenticated;
+      if (!this.isAuthenticated) {
+        this.router.navigate(['/login']);
+      }
+      this.getGradosUsuario();
+    });
+  }
+
+  getGradosUsuario() {
+    let usuarioString = localStorage.getItem('usuario');
+    if (usuarioString != null) {
+      let usuario = JSON.parse(usuarioString);
+      this.grado_service.getGradosUsuario(usuario.id).subscribe(
+        (response) => {
+          this.listGradosUsuario = response;
+        },
+        (error) => {
+          console.log('error: ' + error);
+        }
+      );
+    }
+  }
 }
