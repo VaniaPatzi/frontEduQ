406ba06c5594886226c56544e9ed88a382b64c48
VanPam1 <vanpamep.q@gmail.com>
2023-12-13 17:17:20 -0400

    agregando user

--- src/app/Services/usuario/usuario.service.ts
+++ src/app/Services/usuario/usuario.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  private API_SERVER = 'http://localhost:8080/api/v1/usuarios';
+  private ID_TOKEN = localStorage.getItem('id_token')
+    ? localStorage.getItem('id_token')
+    : '';
+
+  private headers = new HttpHeaders({
+    Authorization: `Bearer ${this.ID_TOKEN}`,
+  });
+  constructor(private httpClient: HttpClient) {}
+
+  public saveUsuario(usuario: any): Observable<any> {
+    return this.httpClient.post(this.API_SERVER, usuario, {
+      headers: this.headers,
+    });
+  }
 }
